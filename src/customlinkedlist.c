3fda4d9eb8332b4ab1cb739f393b9ffd547b6f67
PERLINSKI Eric <eric.perlinski@telecomnancy.net>
2014-11-18 23:08:18 +0100

    Ajout-Modif customlinkedlist

---
+++ src/customlinkedlist.c
@@ -0,0 +1,101 @@
+#include "customlinkedlist.h"
+
+
+List* liste_init(){
+	List* l = (List*)malloc(sizeof(List));
+	l->tete=NULL;
+	l->queue=NULL;
+	l->size=0;
+	return l;
+}
+
+void liste_dest(List* l, FonctionFree fct_free){
+	if(l->size!=0){
+		Elem* e1 = l->tete;
+		Elem* e2 = NULL;
+		int first=1;
+		while(liste_a_un_suivant(l,e1) || first>0){
+
+			e2=e1;
+			if(first>0){
+				first=0;
+			}else{
+				e2=e1;
+				e1 = liste_suivant(l,e1);
+				elem_dest(e2);
+			}
+			((*fct_free)(e1->object));
+		}
+	}
+	free(l);
+}
+
+
+
+void liste_add_last(List* l,void* obj){
+	Elem* el = elem_init(obj);
+	if(l->size==0){
+		l->tete=el;
+		l->queue=el;
+	}else{
+		l->queue->suivant=el;
+		el->precedent=l->queue;
+		l->queue=el;	
+	}
+	l->size++;
+}
+
+Elem* liste_tete(List* l){
+	return l->tete;
+}
+
+
+
+
+
+int liste_a_un_suivant(List* l, Elem* el){
+	return el->suivant!=NULL;
+}
+
+Elem* liste_suivant(List* l, Elem* el){
+	return el->suivant;	
+}
+
+
+void liste_supprime(List* l, Elem* el){
+	Elem* prec = el->precedent;
+	Elem* suc = el->suivant;
+	if(prec == NULL && suc == NULL){
+		l->tete=NULL;
+		l->queue=NULL;
+	}else if(prec == NULL){
+		l->tete=suc;
+		suc->precedent=NULL;
+	}else if(suc==NULL){
+		l->queue=prec;
+		prec->suivant=NULL;
+	}else{
+		prec->suivant=suc;
+		suc->precedent=prec;
+	}
+	l->size--;
+}
+
+
+/**************************************/
+/*              ELEM                  */
+/**************************************/
+
+
+Elem* elem_init(void* obj){
+	Elem* elem = (Elem*)malloc(sizeof(Elem));
+	elem->precedent=NULL;
+	elem->suivant=NULL;
+	elem->object=obj;
+	return elem;
+}
+
+void elem_dest(Elem* el){
+	free(el);
+}
+
---
+++ src/customlinkedlist.h
@@ -0,0 +1,38 @@
+#ifndef CUSTOMLINKEDLIST_H
+#define CUSTOMLINKEDLIST_H
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct element{
+	void* object;
+	struct element* precedent;
+	struct element* suivant;
+} Elem;
+
+typedef struct custom{
+	Elem* tete;
+	Elem* queue;
+	int size;
+} List;
+
+typedef void (* FonctionFree)(void* e);
+
+List* liste_init();
+void liste_dest(List* l, FonctionFree fct_free);
+
+
+void liste_add_last(List* l, void* obj);
+
+Elem* liste_tete(List* l);
+int liste_a_un_suivant(List* l, Elem* el);
+Elem* liste_suivant(List* l, Elem* el);
+void liste_supprime(List* l, Elem* el);
+
+void liste_free(List* l, FonctionFree fct_free);
+
+Elem* elem_init(void* obj);
+void elem_dest(Elem* el);
+
+#endif
