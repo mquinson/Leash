aa8651b2e9571962b1fdc9915a73c0dbe9bfdb61
Valentin GIANNINI <valentin.giannini@telecomnancy.net>
2014-11-23 19:11:29 +0100

    add file for commands implemented (cd, pwd, ...)

---
+++ src/commands_implemented.h
@@ -0,0 +1,14 @@
+#ifndef COMMANDS_IMPLEMENTED
+#define COMMANDS_IMPLEMENTED
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <linux/limits.h>
+#include <unistd.h>
+
+void pwd();
+int cd(char* path);
+
+
+#endif
