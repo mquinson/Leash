0c88164fc5902aaeee3ffc562316cf205489589d
Valentin GIANNINI <valentin.giannini@telecomnancy.net>
2014-12-14 23:33:53 +0100

    commentaire cmd exec

--- src/exec.c
+++ src/exec.c
@@ -1,5 +1,16 @@
 #include "exec.h"
 
+
+/*
+	exec_init
+	A partir d'une chaine de caractere complexe, on decompose les differentes commandes
+	On les decoupe par rapport a |, && et ||
+	le N | M redirige la sortie du N sur l'entre du M
+	le N || M execute M si !N
+	le N && M execute M si N
+
+	les sous commandes seront décomposé dans la pseudo classe 'cmd'
+*/
 Exec* exec_init(Meta* meta,char* exec){
 
 	char* execCpy=(char*)malloc(sizeof(char)*(strlen(exec)+1));
@@ -8,6 +19,7 @@ Exec* exec_init(Meta* meta,char* exec){
 	int i=0;
 	int length=strlen(execCpy);
 	int nbCmd=1;
+	/* On compte le nombre de commande */
 	for(i=0;i<length;i++){
 		if((execCpy[i]=='|' && execCpy[i+1]!='|' && execCpy[i-1]!='|') || (execCpy[i]=='&' && execCpy[i+1]=='&') || (execCpy[i]=='|' && execCpy[i+1]=='|') ){
 			nbCmd++;
@@ -24,11 +36,13 @@ Exec* exec_init(Meta* meta,char* exec){
 			next=0;
 			tabCmdStr[nb] = &execCpy[i];
 		}else if(execCpy[i]=='|' && execCpy[i+1]!='|' && execCpy[i-1]!='|'){
+			/* on a un pipe */
 			execCpy[i]='\0';
 			link[nb]=EXEC_PIPE;
 			nb++;
 			next=1;
 		}else if(execCpy[i]=='&'){
+			/* on a un && */
 			if(i+1<length && execCpy[i+1]=='&'){
 				next=1;
 				execCpy[i]='\0';
@@ -38,6 +52,7 @@ Exec* exec_init(Meta* meta,char* exec){
 				i++;
 			}
 		}else if(execCpy[i]=='|'){
+			/* on a un || */
 			if(i+1<length && execCpy[i+1]=='|'){
 				next=1;
 				execCpy[i]='\0';
@@ -99,6 +114,8 @@ void exec_execute(Exec* exec){
 	int i=0;
 	pipe(fd);
 
+	/* pour chaque cmd, si la sortie n'est pas dans un fichier
+	alors on y branche un tube qui sera examine a la fin de l'execution*/
 	exec->fd_out=fd[0];
 	for(i=0;i<exec->nbCommands;i++){
 		if(exec->link[i]==EXEC_AND || exec->link[i]==EXEC_OR || exec->link[i]==EXEC_NONE ){
@@ -116,9 +133,11 @@ void exec_execute(Exec* exec){
 
 		switch(exec->link[i]){
 			case EXEC_NONE:
+				/* s'il n'y a pas de lien (une cmd, ou a la fin)*/
 				cmd_exec(exec->commands[i]);
 				break;
 			case EXEC_PIPE:
+				/* s'il y a un pipe alors on branche les commandes entre elles*/
 				pipe(fdPipe);
 				if(exec->commands[i]->fd_out==-1){
 					exec->commands[i]->fd_out=fdPipe[1];
@@ -128,6 +147,7 @@ void exec_execute(Exec* exec){
 				leash_close(fdPipe[1]);
 				break;
 			case EXEC_OR:
+				/* s'il y a un 'ou' on continu l'execution si le resultat est correct (0) */
 				cmd_exec(exec->commands[i]);
 				if(exec->commands[i]->result==0){
 					leash_close(fd[1]);
@@ -135,6 +155,7 @@ void exec_execute(Exec* exec){
 				}
 				break;
 			case EXEC_AND:
+				/* s'il y a un 'et' on continu l'execution si le resultat est incorrect (!0) */
 				cmd_exec(exec->commands[i]);
 				if(exec->commands[i]->result!=0){
 					leash_close(fd[1]);
